493362c07ed4034d6aa1c18f355cd0f10e9de814
Kevin Cheng <kc1375@gmail.com>
2017-11-24 17:12:14 -0500

    AAAAAAAAA

--- main.cpp
+++ main.cpp
@@ -10,11 +10,13 @@
 #include <SDL2_image/SDL_image.h>
 #include "Game.hpp"
 
+void quitSDL2() {
+    IMG_Quit();
+    TTF_Quit();
+    SDL_Quit();
+}
 
-int main(int argc, const char * argv[]) {
-    Size windowSize = {1280, 720};
-
-    //init and setup, if something fails, free existing resources and quit the program
+void initSDL2() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "SDL could not init with SDL_INIT_VIDEO. SDL error: " << SDL_GetError() << std::endl;
         return EXIT_FAILURE;
@@ -22,37 +24,38 @@ int main(int argc, const char * argv[]) {
 
     if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
         std::cerr << "SDL2_image could not init with IMG_INIT_PNG. SDL2_image error: " << IMG_GetError() << std::endl;
-        SDL_Quit();
+        quitSDL2()
         return EXIT_FAILURE;
     }
 
-	if(TTF_Init() < 0) {
-		std::cerr << "SDL2_ttf could not init. SDL2_ttf error: " << TTF_GetError() << std::endl;
-		IMG_Quit();
-		SDL_Quit();
-		return EXIT_FAILURE;
-	}
+    if(TTF_Init() < 0) {
+        std::cerr << "SDL2_ttf could not init. SDL2_ttf error: " << TTF_GetError() << std::endl;
+        quitSDL2()
+        return EXIT_FAILURE;
+    }
+}
 
-    SDL_Window *window = SDL_CreateWindow("Potato 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		windowSize.w, windowSize.h, SDL_WINDOW_SHOWN);
+int main(int argc, const char * argv[]) {
+    Size windowSize = {1280, 720};
+
+    //init and setup, if something fails, free existing resources and quit the program
+    initSDL2();
+
+    SDL_Window * window = SDL_CreateWindow("Potato 2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        windowSize.w, windowSize.h, SDL_WINDOW_SHOWN);
 
     if(window == nullptr) {
         std::cerr << "Could not create Window. SDL error: " << SDL_GetError() << std::endl;
-        IMG_Quit();
-		TTF_Quit();
-        SDL_Quit();
+        quitSDL2()
         return EXIT_FAILURE;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,
-		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     if(renderer == nullptr) {
         std::cerr << "Could not create Renderer. SDL error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
-        IMG_Quit();
-		TTF_Quit();
-        SDL_Quit();
+        quitSDL2()
         return EXIT_FAILURE;
     }
 
@@ -64,7 +67,7 @@ int main(int argc, const char * argv[]) {
 
 
     Game *game = new Game(renderer, windowSize);
-	//std::cout << SDL_GetTicks() << std::endl;
+    //std::cout << SDL_GetTicks() << std::endl;
     while(game->run) {
         game->tick();
     }
@@ -82,9 +85,7 @@ int main(int argc, const char * argv[]) {
     renderer = nullptr;
     SDL_DestroyWindow(window);
     window = nullptr;
-    IMG_Quit();
-	TTF_Quit();
-    SDL_Quit();
+    quitSDL2()
     //end program
     return EXIT_SUCCESS;
 }
